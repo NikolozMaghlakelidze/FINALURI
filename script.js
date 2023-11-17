99b23e47595cae52a12c98a12c82b27810c5ce38
NikolozMaghlakelidze <nikolozmaghlakelidze10@gmail.com>
2023-11-17 23:24:37 +0400

    the-new changes
    changed things

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-let burger = document.querySelector(".burger");
-let mark = document.querySelector(".fa-xmark");
-let mobileMenu = document.querySelector(".header_nav");
+let burger = document.querySelector(".fa-bars")
+let mark = document.querySelector(".fa-xmark")
+let menu = document.querySelector(".header_nav")
 
 
-burger.addEventListener("click", ()=>{
+burger.addEventListener("click", () => {
     burger.style.display = "none";
     mark.style.display = "block";
-    mobileMenu.classList.add("show");
-})
+    menu.classList.add("show");
+});
 
 mark.addEventListener("click", () => {
-  burger.style.display = "block";
-  mark.style.display = "none";
-  mobileMenu.classList.remove("show");
+    burger.style.display = "block";
+    mark.style.display = "none";
+    menu.classList.remove("show");
 });
